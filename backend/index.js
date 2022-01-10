f96f828ed6ca5ee16498d41c3b162f981458d72b
Ioannis Fardelas <el13190@central.ntua.gr>
2022-01-10 14:55:28 +0200

    added a new endpoint , works but code is messy

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const app = express();
-
+const vehicle_passes = require("./routes/vehicle_passes");
 const PassesPerStation = require("./routes/PassesPerStation");
 const PassesAnalysis = require("./routes/PassesAnalysis");
 const PassesCost = require("./routes/PassesCost");
 const ChargesBy = require("./routes/ChargesBy");
+// const csvjson = require("./services/JSON2CSV");
 
-
-
-// data requests functional endpoints routing
 app.use("/interoperability/api/PassesAnalysis", PassesAnalysis);
 app.use("/interoperability/api/PassesPerStation", PassesPerStation);
 app.use("/interoperability/api/PassesCost", PassesCost);
 app.use("/interoperability/api/ChargesBy", ChargesBy);
-
-
+app.use("/interoperability/api/vehicle_passes", vehicle_passes);
 
 app.listen(9130, () => {
   console.log("listening live at 9130");
