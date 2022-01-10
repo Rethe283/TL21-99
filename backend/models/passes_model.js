89c311f591c1736f2f57490556c2712557d05c1d
el13133 <el13133@mail.ntua.gr>
2022-01-10 20:57:12 +0200

    added healthcheck endpoint, updated model schema

--- backend/models/passes_model.js
+++ backend/models/passes_model.js
@@ -17,4 +17,34 @@ const Passes = new mongoose.Schema(
   { _id: false, _v: false }
 );
 
+// const pivotSchema = new mongoose.Schema({
+//   'Count of v': String,
+//   p: String,
+//   field3: String,
+//   field4: String
+// })
+
+// const stationSchema = new mongoose.Schema({
+//   stationID: String,
+//   stationProvider: String,
+//   stationName: String,
+// })
+
+// const vehiclesShema = new mongoose.Schema({
+//   vehicleId: String,
+//   tagID: String,
+//   tagProvider: String,
+//   providerAbbr: String,
+//   licenceYear: String
+// })
+
+
+// const Passes = mongoose.model('Pass', Passes)
+// const Pivot = mongoose.model('Pivot', pivotSchema)
+// const Station = mongoose.model('Station', stationSchema)
+// const Vehicles = mongoose.model('Vehicles', vehiclesShema)
+
+// module.exports = { Passes, Pivot, Station, Vehicles }
+
+
 module.exports = mongoose.model("Pass", Passes);
