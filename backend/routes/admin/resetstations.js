272323c12633750832b5f7399c13273a83599986
Marily  Vlontzou <marilyvlontzou@Marilys-MacBook-Pro.local>
2022-01-11 17:16:49 +0200

    delete require mongoose

--- backend/routes/admin/resetstations.js
+++ backend/routes/admin/resetstations.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const mongoose = require('mongoose')
+
 
 const stationsdata = require('../../Data/stations')
 const Station = require('../../models/Stations')
