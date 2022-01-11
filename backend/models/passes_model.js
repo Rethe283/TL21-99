c27346d3a0153f3625a3005fb51db63c71aad763
el13133 <el13133@mail.ntua.gr>
2022-01-11 09:57:56 +0200

    added stations, vehicles, pivot models | pivot model doesnt work

--- backend/models/passes_model.js
+++ backend/models/passes_model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/softeng21-99");
+console.log("connected to database");
 
 const Passes = new mongoose.Schema(
   {
@@ -16,35 +17,5 @@ const Passes = new mongoose.Schema(
   },
   { _id: false, _v: false }
 );
-
-// const pivotSchema = new mongoose.Schema({
-//   'Count of v': String,
-//   p: String,
-//   field3: String,
-//   field4: String
-// })
-
-// const stationSchema = new mongoose.Schema({
-//   stationID: String,
-//   stationProvider: String,
-//   stationName: String,
-// })
-
-// const vehiclesShema = new mongoose.Schema({
-//   vehicleId: String,
-//   tagID: String,
-//   tagProvider: String,
-//   providerAbbr: String,
-//   licenceYear: String
-// })
-
-
-// const Passes = mongoose.model('Pass', Passes)
-// const Pivot = mongoose.model('Pivot', pivotSchema)
-// const Station = mongoose.model('Station', stationSchema)
-// const Vehicles = mongoose.model('Vehicles', vehiclesShema)
-
-// module.exports = { Passes, Pivot, Station, Vehicles }
-
 
 module.exports = mongoose.model("Pass", Passes);
