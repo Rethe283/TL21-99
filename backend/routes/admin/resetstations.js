b974fc14e7ca021a66d4531079cf2a4b7fc27f08
Chrysa712 <chrizeakoy@gmail.com>
2022-01-11 16:35:09 +0200

    reset stations

--- backend/routes/admin/resetstations.js
+++ backend/routes/admin/resetstations.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require('mongoose')
 
-router.post('/', (req, res) => {
+const stationsdata = require('../../Data/stations')
+const Station = require('../../models/Stations')
 
-    res.status(200).send({"status":"OK"})
+router.post('/',  async (req, res) => {
+    try{
+        await Station.deleteMany() 
+        await Station.insertMany(stationsdata)
+        res.status(200).send({status: "OK"})
+    } catch(err){
+        res.status(500).send({status: "failed", msg: err.message})
+    }
+
 })
 
 module.exports = router
+
