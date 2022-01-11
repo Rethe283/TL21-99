f705f97406090b986b4e2e8f44596331644007f7
Nikolaos Spiliopoulos <Spiliopn@gmail.com>
2022-01-11 15:38:03 +0200

    commit

--- backend/routes/admin/resetvehicles.js
+++ backend/routes/admin/resetvehicles.js
@@ -1,8 +1,25 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require('mongoose')
+const Vehicles = require('../../models/Vehicles')
+const vehiclejson = require('../../Data/vehicles')
+const resetvehicles = async (req, res, next) => {
+    const areVehiclesReset = await resetAllVehicles()
+    if (areVehiclesReset === false) {
+        res.send({ status: 'failed' })
+    }
+    res.send({ status: 'OK' })
+}
 
-router.get('/', (req, res) => {
-    res.send({msg: 'im in resetvehicles'})
-})
 
-module.exports = router
+
+const resetAllVehicles = async () => {
+    const deleteTheVehicles = await Vehicles.deleteMany()
+    const resetTheVehicles = await Vehicles.insertMany(vehiclejson, {vehicles: []})
+    if (deleteTheVehicles && resetTheVehicles) return true
+    else return false
+}
+
+
+
+module.exports = resetvehicles
