345fac5f06354667efed4b2785ea225f144ed797
el13133 <el13133@mail.ntua.gr>
2022-01-11 12:47:07 +0200

    structure of admin endpoints ready

--- backend/index.js
+++ backend/index.js
@@ -12,21 +12,21 @@ const settlement = require("./routes/settlement");
 // admin endpoints include
 const healthcheck = require("./routes/admin/healthcheck")
 const resetpasses = require("./routes/admin/resetpasses")
-// const resetstations = require("./routes/admin/resetstations")
-// const resetvehicles = require("./routes/admin/resetvehicles")
+const resetstations = require("./routes/admin/resetstations")
+const resetvehicles = require("./routes/admin/resetvehicles")
 
+// app.use("/interoperability/api/ChargesBy", ChargesBy);
 app.use("/interoperability/api/PassesAnalysis", PassesAnalysis);
 app.use("/interoperability/api/PassesPerStation", PassesPerStation);
 app.use("/interoperability/api/PassesCost", PassesCost);
-// app.use("/interoperability/api/ChargesBy", ChargesBy);
 app.use("/interoperability/api/vehicle_passes", vehicle_passes);
 app.use("/interoperability/api/settlement", settlement);
 
 // admin endpoints routing
 app.use("/interoperability/api/admin/healthcheck", healthcheck)
 app.use("/interoperability/api/admin/resetpasses", resetpasses)
-// app.use("/interoperability/api/admin/resetstations", resetstations)
-// app.use("/interoperability/api/admin/resetvehicles", resetvehicles)
+app.use("/interoperability/api/admin/resetstations", resetstations)
+app.use("/interoperability/api/admin/resetvehicles", resetvehicles)
 
 app.listen(9130, () => {
   console.log("listening live at 9130");
