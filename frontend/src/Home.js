f335b98a27111bd04d2bbcae85cca6c3d8f7174a
el13133 <el13133@mail.ntua.gr>
2022-01-19 22:38:49 +0200

    fetch endpoint data trial1

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,9 +1,22 @@
+import useFetch from "./useFetch";
+
 const Home = () => {
+  const { error, isPending, data } = useFetch(
+    "http://localhost:9130/interoperability/api/ChargesBy/EG/20190101/20200101"
+  );
 
-    
-    return (
-        <p>Lorem ipsum dolor sit amet.</p>
-    )
-}
+  return (
+    <div className="home">
+      {error && <div> {error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && (
+        <div>
+          <p>This is intraPass</p>
+          <p>{data}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Home 
+export default Home;
