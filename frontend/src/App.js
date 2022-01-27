ecb64415070c665636212d2fc2fb76eef52bc7b2
Nikolaos Spiliopoulos <Spiliopn@gmail.com>
2022-01-27 21:42:24 +0200

    fix structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,48 @@
-import Navbar from "./Navbar";
-import Home from "./Home.js";
+// import Navbar from "./Navbar";
+// import Home from "./Home.js";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Payment from "./Payment";
+// import Analysis from "./Analysis";
+// import ConfirmPayment from "./pages/ConfirmPayment"
+// import Redirect from "./Redirect"
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Navbar></Navbar>
+//         <div className="content">
+//           <Switch>
+//             <Route exact path="/">
+//               <Home></Home>
+//             </Route>
+//             <Route exact path="/payment">
+//               <Payment></Payment>
+//             </Route>
+//             <Route path="/analysis">
+//               <Analysis></Analysis>
+//             </Route>
+//             <Route path='/confirmPayment'>
+//               <ConfirmPayment></ConfirmPayment>
+//             </Route>
+//             <Route path='/redirecting'>
+//               <Redirect></Redirect>
+//             </Route>
+//           </Switch>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Payment from "./Payment";
-import Analysis from "./Analysis";
-import ConfirmPayment from "./ConfirmPayment"
-import Redirect from "./Redirect"
+import Payment from "./pages/Payment";
+import Analysis from "./pages/Analysis";
+import ConfirmPayment from "./pages/ConfirmPayment"
+import Redirect from "./pages/Redirect"
 
 function App() {
   return (
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
 
-const MainPage = () => {
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Welcome to IntraPass</h1>
+//       <p>
+//       You can use this web app to view our <Link to="/Analysis" className="link">Analysis</Link> and make <Link to="/Payment" className="link">Payments</Link>
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Home;
+import useFetch from "../useFetch";
+
+const Home = () => {
+  const { error, isPending, data } = useFetch(
+    "http://localhost:9130/interoperability/api/ChargesBy/EG/20190101/20200101"
+  );
+
   return (
-    <div>
-      <h1>Welcome to IntraPass</h1>
-      <p>
-      You can use this web app to view our <Link to="/Analysis" className="link">Analysis</Link> and make <Link to="/Payment" className="link">Payments</Link>
-      </p>
+    <div className="home">
+      {error && <div> {error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && (
+        <div>
+          <p>This is intraPass</p>
+          <p>{data}</p>
+        </div>
+      )}
     </div>
   );
 };
 
-export default MainPage;
+export default Home;
