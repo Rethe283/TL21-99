bfb04d7f891e1e7dececa70aa9166abbcf347590
el13133 <el13133@mail.ntua.gr>
2022-01-27 20:01:31 +0200

    works till now

--- frontend/src/Analysis.js
+++ frontend/src/Analysis.js
@@ -1,5 +1,12 @@
+import { useState } from "react/cjs/react.development";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 const Analysis = () => {
-    return ( <h1>Analysis fine</h1> );
-}
- 
+  return (
+    <div className="analysis">
+      <h2>Passes report</h2>
+      </div>
+  );
+};
+
 export default Analysis;
