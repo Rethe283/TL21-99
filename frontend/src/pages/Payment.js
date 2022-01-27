18bacdd31ab46eee3a0a0009caff83e2f8aeea62
Nikolaos Spiliopoulos <Spiliopn@gmail.com>
2022-01-27 20:45:39 +0200

    a

--- frontend/src/pages/Payment.js
+++ frontend/src/pages/Payment.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect
-} from "react-router-dom";
+// TODO:
+//  - date from earlier than date to 
+//  
 
-import Home from './Home';
-import PaymentData from '../components/PaymentData';
+import { useState } from "react/cjs/react.development";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Payment = () => {
-    return(
-        <div>
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/payment/data" component={PaymentData}/>
-                    <Redirect to="/payment"/>
-                </Switch>
-            </Router>
-        </div>
-    )
-}
+  const [dateFrom, setDateFrom] = useState("");
+  const [dateTo, setDateTo] = useState("");
+  const [payingOperator, setPayingOperator] = useState("");
+  const [receivingOperator, setReceivingOperator] = useState("");
+  const history = useHistory()
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+    // fetch data, redirect
+    console.log(dateFrom);
+    history.push('/confirmPayment')
+  };
+
+  return (
+    <div className="payment">
+      <h2> Make payment</h2>
+      <form onSubmit={handleSubmit}>
+        <h3>Dates</h3>
+        <label>from</label>
+        <input
+          type="date"
+          required
+          value={dateFrom}
+          onChange={(e) => setDateFrom(e.target.value)}
+        />
+        <label>to</label>
+        <input
+          type="date"
+          required
+          value={dateTo}
+          onChange={(e) => setDateTo(e.target.value)}
+        />
+        <br />
+
+        <h3>Organisation</h3>
+
+        <label htmlFor="">paying</label>
+        <select
+          value={payingOperator}
+          required
+          onChange={(e) => {
+            setPayingOperator(e.target.value);
+          }}
+        >
+          <option value="AO">aodos</option>
+          <option value="GF">gefyra</option>
+          <option value="EG">egnatia</option>
+          <option value="KO">kentriki odos</option>
+          <option value="MR">moreas</option>
+          <option value="NE">nea odos</option>
+          <option value="OO">olympia odos</option>
+        </select>
+
+        <label htmlFor="">receiving</label>
+        <select
+          value={receivingOperator}
+          required
+          onChange={(e) => {
+            setReceivingOperator(e.target.value);
+          }}
+        >
+          <option value="AO">aodos</option>
+          <option value="GF">gefyra</option>
+          <option value="EG">egnatia</option>
+          <option value="KO">kentriki odos</option>
+          <option value="MR">moreas</option>
+          <option value="NE">nea odos</option>
+          <option value="OO">olympia odos</option>
+        </select>
+        <br />
+        <button type="submit">proceed</button>
+      </form>
+    </div>
+  );
+};
 
 export default Payment;
